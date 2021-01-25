cc1546224f6b8f914b133c800e10dfbc197707e0
Julius Shapiro <jkurt@Juliuss-MBP.localdomain>
2021-01-25 05:37:56 -0600

    see readme

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -18,6 +18,8 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
+    axios
+    .put('http://localhost:5000/api/colors:' + { }, updateColors)
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
