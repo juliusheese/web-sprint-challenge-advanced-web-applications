87da318d9ba35e4a4acb5c33b13f9abffdc1a53b
Julius Shapiro <jkurt@Juliuss-MBP.localdomain>
2021-01-25 08:28:06 -0600

    better

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,38 +1,24 @@
-import React, { useState, useEffect, componentDidMount } from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import {token} from './Login'
 
-//import {axiosWithAuth} from "../axiosAuth";
-import axios from 'axios';
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
-import {token} from "./Login"
-const credentials = {username: 'Lambda School', password: 'i<3Lambd4'};
-
-const axiosWithAuth = axios.create({
-  baseURL: `http://localhost:5000/api/colors`,
-  headers: {
-    Authorization: `Bearer + ${token}`
-  },      
-   body: credentials
-});
 
 const BubblePage = () => {
-
-
   const [colorList, setColorList] = useState([]);
-  axiosWithAuth
-    .get('http://localhost:5000/api/colors')
-    .then(response => console.log(response))
-    .then(response => console.log(response))
-    .then(response => setColorList(response))
-   // .then(console.log(colorList));
-
-
-
+  console.log(token);
+  axios
+  .get('http://localhost:5000/api/colors', token)
+  .then(response => console.log(response))
+  .then(console.log(token))
+  .catch(error => {
+    console.log(error)
+  })
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
   return (
-
     <>
       <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
@@ -41,3 +27,11 @@ const BubblePage = () => {
 };
 
 export default BubblePage;
+
+
+
+
+
+
+
+
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,45 +1,43 @@
-import React, { useState, createContext } from 'react';
+import React, { useState } from 'react';
 //import { axiosWithAuth } from '../axiosAuth';
 //import BubblePage from "./BubblePage";
 import axios from 'axios';
+import BubblePage from './BubblePage';
+import { render } from '@testing-library/react';
 
-
-const AuthContext = React.createContext();
-export let token = '';
 const credentials= { username: 'Lambda School', password: 'i<3Lambd4' }
-
+let token;
 const Login = () => {
+
 
   //preventDefault();
 
   axios
     .post('http://localhost:5000/api/login ', credentials)
     .then(res => {
-      console.log(res);
-      token = res.data;
-      console.log(res.data);
+       token = res.data;
+      console.log(token);
     })
     .catch(error => {
       console.log(error)
-    })
+    });
 
-
-
+    
 
 
 
   return (
     /** */
-    <form>
+    <div>
       <div >
         <input type="username" value={credentials.username} />
       </div>
       <div >
         <input type="password" value={credentials.password} />
       </div>
-      <button type="submit" >Submit </button>
-    </form>
+      <button onClick={() => render(<BubblePage></BubblePage>)} >Submit </button>
+     </div>
   )
 }
-
+export {token};
 export default Login;
