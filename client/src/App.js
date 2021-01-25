e49d37dce8b67d2bb6226c424aacd26a87c8c260
Julius Shapiro <jkurt@Juliuss-MBP.localdomain>
2021-01-25 01:57:31 -0600

    fininshing axios stuff

--- client/src/App.js
+++ client/src/App.js
@@ -4,12 +4,26 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login";
 import "./styles.scss";
 
+const PrivateRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem("token") ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to="/login" />
+      )
+    }
+  />
+);
+
 function App() {
   return (
     <Router>
       <div className="App">
         <Route exact path="/" component={Login} />
-        {/* 
+        {<PrivateRoute path='/bubblepage' component={Protected} />
+        /* 
           Build a PrivateRoute component that will 
           display BubblePage when you're authenticated 
         */}
